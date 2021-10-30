c2c40c6b53cc29ad545836e98cc584a659c45b4b
Gagniuc <88835962+Gagniuc@users.noreply.github.com>
2021-10-30 18:19:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//  ________________________________                          ____________________________
+// /  PromKappa                     \________________________/       v2.00                |
+// |                                                                                      |
+// |                   Name:  PromKappa (ported from VB6 to PHP)                          |
+// |     Original language :  Visual Basic 6                                              |
+// |    Original source V1 :  http://www.biomedcentral.com/1471-2164/13/512/additional    |
+// |    Original source V2 :  http://www.biomedcentral.com/1471-2164/14/278/additional    |
+// |               Category:  Open source software                                        |
+// |                 Author:  Paul A. Gagniuc                                             |
+// |                  Email:  paul_gagniuc@acad.ro                                        |
+// |                                                                                      |
+// |           Date Created:  October 2013                                                |
+// |               Language:  PHP                                                         |
+// |                    Use:  DNA patterns, analysis of DNA sequences                     |
+// |                                                                                      |
+// |                  _____________________________                                       |
+// |_________________/                             \______________________________________|
+//
+
+?>
+
+<img src="ods.php?s=AGCATGCTATCTTCATACTACTACTAGCTATCTACTATCGTATCATCTATCTGTCTACTTACACTACTCACTCATACTCA">
